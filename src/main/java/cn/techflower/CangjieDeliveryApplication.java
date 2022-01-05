9473bfcbd477a9b0f5990fbfd294c56ee27a4039
yijian <yijian@qunhemail.com>
2022-01-05 22:15:17 +0800

    feat(init): init
    init

---
+++ src/main/java/cn/techflower/CangjieDeliveryApplication.java
@@ -0,0 +1,13 @@
+package cn.techflower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CangjieDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CangjieDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/techflower/CangjieDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.techflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CangjieDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
